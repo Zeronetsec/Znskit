83d517f639dd99d86610d54d58bf056333a4ac32
Zeronetsec <senxhunter996@gmail.com>
2026-07-18 03:34:37 +0700

    modified: .install/include.sh

--- .install/include.sh
+++ .install/include.sh
@@ -1,39 +1,39 @@
 function include() {
-    local input_data
-    local line
-    local ecode
-
     if [[ "${1}" != ":" ]]; then
         return 1
     fi
 
     shift
-    input_data="${1}"
-
+    local input_data="${1}"
     if [[ -z "${input_data}" ]]; then
         return 1
     fi
 
-    input_data="$(
-        echo -e "${input_data}" | \
-        command sed -n '/(/,/)/ {
-            s/[()]//g;
-            s/^[[:space:]]*//;
-            s/[[:space:]]*$//;
-            /^$/d;
-            p;
-        }'
-    )"
+    local line
+    local inside_bracket=0
 
     while read -r line; do
-        [[ -z "${line}" ]] && continue
-        [[ "${line}" =~ ^# ]] && continue
-        if [[ ! -f "${root}/${line}.sh" ]]; then
-            echo -e "\x1b[1;31m[!] \x1b[0mInclude: \x1b[0;32m${line} \x1b[0mnot found!"
-            return 1
+        line="${line#"${line%%[![:space:]]*}"}"
+        line="${line%"${line##*[![:space:]]}"}"
+        if [[ "${line}" == *"("* ]]; then
+            inside_bracket=1
+            continue
         fi
-        source "${root}/${line}.sh"
-    done <<< "${input_data}"
 
+        if [[ "${line}" == *")"* ]]; then
+            inside_bracket=0
+            continue
+        fi
+
+        if (( inside_bracket )); then
+            [[ -z "${line}" ]] && continue
+            [[ "${line}" =~ ^# ]] && continue
+            if [[ ! -f "${root}/${line}.sh" ]]; then
+                echo -e "\x1b[1;31m[!] \x1b[0mInclude: \x1b[0;32m${line} \x1b[0mnot found!"
+                return 1
+            fi
+            source "${root}/${line}.sh"
+        fi
+    done <<< "${input_data}"
     return 0
-}; readonly -f include
+}
