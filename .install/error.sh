250c1104c30097223ab05ca15fde363b77e66a18
Zeronetsec <senxhunter996@gmail.com>
2026-08-29 04:28:15 +0700

    modified: .install/error.sh

--- .install/error.sh
+++ .install/error.sh
@@ -1,5 +1,11 @@
 function install::error() {
+    local cmd="${BASH_COMMAND}"
+
     echo -e " ${DG}- ${N}error: ${R}${?}${N}"
+
+    if [[ "${__ERRSHOW__}" == true ]]; then
+        echo -e "${N}${cmd}"
+    fi
 }; readonly -f install::error
 
-trap install::error EXIT
+trap install::error ERR EXIT
